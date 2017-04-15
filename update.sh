6ac79e3ad69413c39ba7919400e5004b3d65fc4b
Administrator <admin@example.com>
2017-04-15 11:35:31 +0300

    ok

--- update.sh
+++ update.sh
@@ -1,9 +1,4 @@
 git add .
 git commit -m"ok"
-<<<<<<< HEAD
 git push origin master
 git push origing master
-=======
-git pull origin master
-git push origin master
->>>>>>> 951736f55e7c979a706b99f1646046e2200c3aea
